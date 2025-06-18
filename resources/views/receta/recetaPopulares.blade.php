aeb2dc13b14a364d988f02861ca535af3472f857
Jaione2502 <jaldaregia@tigloo.es>
2025-06-18 16:27:51 +0200

    subida ficheros TFM1
    subida ficheros TFM1

---
+++ resources/views/receta/recetaPopulares.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.main')
+
+@section('title','Recetas de cocina')
+
+@section('content')
+
+
+ <main class="section">
+        <h1>Recetas Populares</h1>
+        <div id="recipe-container" class="recipe-container">
+           
+        </div>
+    </main>
+
+@endsection
